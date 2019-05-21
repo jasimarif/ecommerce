65af27b11f90534c8b74e903bd44e33b12062f90
Xzib <syedzohaibali94@gmail.com>
2019-05-22 01:49:31 +0500

    Update login.php
    basic login done, Tum apnay pas xampp me database bnao with databasename `SignUp` and table name `cust_info` and three columns `id` `email` `pass` phr ye test kro login or btao further kya dalun. abhi isme cookie or session nhi h

--- login.php
+++ login.php
@@ -1,8 +1,9 @@
 <?php
 
+    session_start();
     $error="";
     if(array_key_exists('email',$_POST)){
-        print_r($_POST);
+       
         if(!$_POST['email']){
             $error .= "<p>Please Enter an Email ID<br></p>";
         }
@@ -13,17 +14,38 @@
             $error = "<p>There were errors in your Form\n</p>".$error;
         }else{
             $link = mysqli_connect("localhost","root","","SignUp");
-            $query = "SELECT `ID` FROM `cust_info` 
-            WHERE `email` = '".mysqli_real_escape_string($link,$_POST['email'])."'LIMIT 1";
-            $result = mysqli_query($link,$query);
-            if(mysqli_num_rows($result) > 0){
-                $error .= "<p> This id is already signed up </p> <br>";
-            }else{
-                $var = $_POST['password'];  
-                $query = "INSERT INTO `cust_info`(`email`,`pass`) 
-                VALUES('".mysqli_real_escape_string($link,$_POST['email'])."','".mysqli_real_escape_string($link,$var)."')";
+            if($_POST['signup']==1){
+               
+            	 $query = "SELECT `ID` FROM `cust_info` 
+                WHERE `email` = '".mysqli_real_escape_string($link,$_POST['email'])."'LIMIT 1";
                 $result = mysqli_query($link,$query);
-            } 
+                if(mysqli_num_rows($result) > 0){
+                    $error .= "<p> This id is already signed up </p> <br>";
+                }
+                else{
+                    $var = $_POST['password'];  
+                    $query = "INSERT INTO `cust_info`(`email`,`pass`) 
+                    VALUES('".mysqli_real_escape_string($link,$_POST['email'])."','".mysqli_real_escape_string($link,$var)."')";
+                    $result = mysqli_query($link,$query);
+                }
+            }
+            else{
+                
+                $enteredPass = $_POST['password'];
+                $query = "SELECT * FROM `cust_info`
+                Where `email` = '".mysqli_real_escape_string($link,$_POST['email'])."'";
+                $result = mysqli_query($link,$query);
+                $row = mysqli_fetch_array($result);
+                if($enteredPass == $row["pass"]){
+                    header("Location: index.php");
+                }
+                else{
+                    $error .= "<p> Incorrect Email ID or Password<br></p>";
+                }
+                
+
+            }
+             
         }
         
     }
@@ -44,11 +66,24 @@
           <p><?php echo $error; ?></p>  
         </div>
         <form method="POST">
-
             <input name="email" type="email" placeholder="abc@xyz.com">
-            <input name="password" type="password" placeholder="Enter your password"> 
+            <input name="password" type="password" placeholder="Enter your password">
+            <input name="signup" type="hidden" value="1"> 
+            <input name="keeploggedin" type="checkbox" value="1"> 
             <input name="submit" type="submit" value="SignUp">
         </form>
+        <p></p>
+
+        
+        <form method="POST">
+            <input name="email" type="email" placeholder="abc@xyz.com">
+            <input name="password" type="password" placeholder="Enter your password"> 
+            <input name="signup" type="hidden" value="0"> 
+            <input name="keeploggedin" type="checkbox" value="1">
+            <input name="submit" type="submit" value="Log in">
+        </form>
+        
+        
 
     </body>
 </html>
