bc07a49973a8dc8eca1084b6d84dc0398babc1e9
jasimarif <jasimarifali@gmail.com>
2019-05-21 20:40:04 +0500

    main page

---
+++ index.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+   
+     
+      <!--font awsome-->
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+      
+  
+  
+       <link rel="stylesheet" type="text/css" href="style.css" media="all">
+     <title>Tijarat</title>
+      
+    </head>
+  <body>
+    
+      
+        
+    
+    <div class="wrapper"> 
+        
+        <nav id="sidebar" class="container-fluid"> 
+            <div class="sidebar-header"> 
+                <h3> Categories </h3>
+            </div>
+            <ul class="list-unstyled components">
+                
+                            
+                <li class="active"> 
+                <a href="#mens_menu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> Men </a>
+                            
+                    <ul class="collapse list-unstyled"  id="mens_menu">
+                        <li> <a href="#"> Top picks </a> </li>
+                        <li> <a href="#"> Kurta Shalwar</a> </li>
+                        <li> <a href="#"> Waistcoat</a> </li>
+                        <li> <a href="#"> Unstitched fabric</a> </li>
+                    
+                    </ul>
+                    </li>
+                
+                <li> 
+                <a href="#womens_menu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> Women </a>
+                            
+                    <ul class="collapse list-unstyled"  id="womens_menu">
+                        <li> <a href="#"> Top picks </a> </li>
+                        <li> <a href="#"> Kurta Shalwar</a> </li>
+                        <li> <a href="#"> Waistcoat</a> </li>
+                        <li> <a href="#"> Unstitched fabric</a> </li>
+                    
+                    </ul>
+                    </li>
+                
+                <li> 
+                <a href="#"> Kids </a>
+                </li>
+                <li> 
+                <a href="#"> New Arrival </a>
+                </li>
+
+            </ul>
+            
+          
+          </nav>
+    
+        <!--NAVBAR SETTINGS-->   
+      
+          <div class="contents container-fluid">
+          <nav class="navbar navbar-expand-xl navbar-light" style="background-color: white;">
+              
+              
+                  
+    <button type="button" id="sidebarCollapse" class="btn btn-light btn-sm"> 
+        <i class="fas fa-align-justify">  </i>
+
+    </button>
+        
+        <a class="navbar-brand" href="#">TiJARAT</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse " id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+        
+        <li class="nav-item  ">
+        <a class="nav-link" href="#">My Account <span class="sr-only">(current)</span></a>
+      </li>
+        
+     
+      <li class="nav-item  dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categories
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+          
+      
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Shopping Cart <span class="sr-only">(current)</span></a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">About Us<span class="sr-only">(current)</span></a>
+      </li>
+        
+    </ul>
+     
+    <form method="get" action="results.php"  enctype="multipart/form-data" class="form-inline form-group my-2 my-lg-0">
+        
+      <input class="form-control mr-sm-2" name="user_query" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn my-2 my-sm-0" type="submit" id="submit">
+     <i class="fas fa-search"></i>
+
+    </button>
+    </form>
+         
+      
+  <form class="form-inline">
+    <button class="btn btn-sm mx-4 btn-outline-success" type="button">Sign Up</button>
+    <button class="btn btn-sm btn-outline-primary" type="button">Sign In</button>
+  </form>
+
+  </div>
+                       
+</nav>  
+          </div>   
+</div>
+        
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+
+      <script>   
+      
+      $(document).ready(function(){
+          
+            $('#sidebarCollapse').on('click',function(){
+                
+                $('#sidebar').toggleClass('active')
+                
+                
+            }); 
+          
+      });
+      
+      
+      
+      </script>
+      
+      
+      
+      
+      
+      
+      
+  
+    
+    </body>
+</html>
