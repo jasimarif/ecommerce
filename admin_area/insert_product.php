2e8ee294c4678d1eef548a9dc6c83d6200941940
jasimarif <48030880+jasimarif@users.noreply.github.com>
2019-05-24 06:36:53 +0500

    queries inserted

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -1,4 +1,11 @@
 <html>
+    
+    <?php
+
+   include("db.php");
+        
+?>
+    
 <head>
     
     <title> Insert products</title>
@@ -18,42 +25,68 @@
         
         <tr> 
             <td align="right"><b> Product Title: </b></td>
-            <td> <input type="text" name="product_title"> </td>
+            <td> <input type="text" name="product_title" size="60" required> </td>
+        <tr> 
+             <td align="right"> <b>Product type:</b> </td>
+            <td> <select name="product_type" id="product_type" required>
+                <option> Men </option>
+                <option> Women </option>
+                <option> Kids </option>
+                <option> New Arrival </option>
+                
+                </select>
+            
+            </td>
         
             
         </tr>
+            
+        
          <tr> 
              <td align="right"><b> Product Category:</b> </td>
-            <td> <select name="product_cat">  
-                <option> select a category </option>
-                            <option> <?php  ?> </option>
+            <td> <select  name="product_cat" required>  
+                <option> Select a category </option>
+                
+                <?php 
+    $get_men= "Select * FROM `men` ";
+
+    $query_men = mysqli_query($con, $get_men);
+
+    while ($row_men=mysqli_fetch_array($query_men))
+    {
+        $men_id= $row_men['id'];
+        $men_type= $row_men['type'];
+
+        echo "<option value='id'> $men_type </option>" ;
+    } ?>
                 
                 
                 </select> </td>
         
             
         </tr>
-         <tr> 
-             <td align="right"> <b>Product Brand:</b> </td>
-            <td> <input type="text" name="product_title"> </td>
-        
-            
-        </tr>
+         
          <tr> 
              <td align="right"><b> Product Image: </b></td>
-            <td> <input type="text" name="product_title"> </td>
+            <td> <input type="file" name="product_image" required> </td>
         
             
         </tr>
          <tr> 
              <td align="right"><b> Product Description: </b></td>
-            <td> <input type="text" name="product_title"> </td>
+             <td> <textarea cols="20" rows="10" name="product_desc">   </textarea> </td>
+        
+            
+        </tr>
+        <tr> 
+            <td align="right"> <b>Product Price: </b></td>
+            <td> <input type="text" name="product_price" required> </td>
         
             
         </tr>
          <tr> 
             <td align="right"> <b>Product Keywords: </b></td>
-            <td> <input type="text" name="product_title"> </td>
+            <td> <input type="text" name="product_keywords" size="50" required> </td>
         
             
         </tr>
@@ -68,6 +101,36 @@
         </table>
     
     </form>
+    
+    
+    <?php 
+    
+    if(isset($_POST['insert_post']))
+    {
+        
+        $product_cat = $_POST['product_cat'];
+        $product_type = $_POST['product_type'];
+        $product_title = $_POST['product_title'];
+        $product_price = $_POST['product_price'];
+        $product_desc = $_POST['product_desc'];
+       
+        $product_keywords = $_POST['product_keywords'];      
+       
+         $product_image = $_FILES['product_image'] ['name'];
+         $product_image_tmp = $_FILES['product_image']['tmp_name'];
+        
+       echo $insert_query="INSERT into `products` ('product_cat','product_type','product_title','product_price','product_desc','product_image','product_keywords') 
+        values ('$product_cat','$product_type','$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
+        
+
+    }
+    
+    
+    
+    ?>
+    
+    
+    
     
     </body>
 
