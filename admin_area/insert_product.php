e939aa8186862c4a66bcf167f8b6ac56b807c0d8
jasimarif <jasimarifali@gmail.com>
2019-05-26 17:50:23 +0500

    products inserted into db

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -119,8 +119,18 @@
          $product_image = $_FILES['product_image'] ['name'];
          $product_image_tmp = $_FILES['product_image']['tmp_name'];
         
-       echo $insert_query="INSERT into `products` ('product_cat','product_type','product_title','product_price','product_desc','product_image','product_keywords') 
+        move_uploaded_file($product_image_tmp, "product_images/$product_image");
+        
+        $insert_product="INSERT into `products` (product_cat,product_type,product_title,product_price,product_desc,product_image,product_keywords) 
         values ('$product_cat','$product_type','$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
+       
+        $insert_pro= mysqli_query($con,$insert_product);
+        
+        if($insert_pro)
+        {
+            echo "<script> alert ('The product has been inserted!')</script>";
+            echo "<script> window.open('insert_product.php', '_self')</script>";
+        }
         
 
     }
