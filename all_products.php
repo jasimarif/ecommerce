2ffd479010db23aad2694fa73f033bcf249695e0
Xzib <syedzohaibali94@gmail.com>
2019-07-07 19:30:31 +0500

    allproducts page modified with cards

--- all_products.php
+++ all_products.php
@@ -21,39 +21,66 @@ session_start();
       <!--WelcomeBar-->
       <?php include("includes/welcome_guest.php");   ?>
 
-        <div id="product_box"> 
-        
-            <h3 id="products"> Products </h3>
-            
-             <?php 
-               $get_pro= "SELECT * FROM products";
-    $run_pro= mysqli_query($con, $get_pro);
-    while($row_pro=mysqli_fetch_array($run_pro))
-    {
-        $pro_id=$row_pro['product_id'];
-        $pro_cat=$row_pro['product_cat'];
-        $pro_type=$row_pro['product_type'];
-        $pro_title=$row_pro['product_title'];
-        $pro_price=$row_pro['product_price'];
-        $pro_image=$row_pro['product_image'];
-
-        echo "
-           
-            
-            <div id=single_product> 
-            <a href='details.php?pro_id=$pro_id' >  <p>  $pro_title  </p> </a>
-            <a href='details.php?pro_id=$pro_id'> <img src='admin_area/product_images/$pro_image' width='180' height='200' /> </a>
-            <p> PKR $pro_price </p>
-            <a href='all_products.php?add_cart=$pro_id'> <button class='btn btn-sm mx-4 btn-primary' style:'float:right'> Add to cart </button> </a>     
+      <div id="product_box"  class="" style="margin-top:60px;background-color:#F5F5F5" >
+          <div class="container-flex">
+            <div class="col-sm-12">
+              <div class="row">
+                 
+                    <?php 
+                    $get_pro= "SELECT * FROM products";
+         $run_pro= mysqli_query($con, $get_pro);
+         while($row_pro=mysqli_fetch_array($run_pro))
+         {
+             $pro_id=$row_pro['product_id'];
+             $pro_cat=$row_pro['product_cat'];
+             $pro_type=$row_pro['product_type'];
+             $pro_title=$row_pro['product_title'];
+             $pro_price=$row_pro['product_price'];
+             $pro_image=$row_pro['product_image'];
+     
+             echo "
                 
-            </div>
-        
-        ";
-    }
-           all_pro_cart();
-        
-            ?> 
-        
+                 
+             <div class='col-sm-3 my-2 singleProduct' onmouseover='border()'>    
+             <div class='card' style='width: 18rem;'>
+             <a href='details.php?pro_id=$pro_id'><img class='card-img-top' src='admin_area/product_images/$pro_image' alt='$pro_title image'  width='180' height='200'></a>
+             <div class='card-body'>
+               <h5 class='card-title'>$pro_title</h5>
+             </div>
+             <div class='card-body text-center' style='margin-top:-45px; color:orange; font-size:20px;'>
+               $pro_price
+             </div>
+             
+             <div class='card-body' style='margin-top:-35px'>
+               <a href='index.php?add_cart=$pro_id' class='card-link'> <button class='btn btn-sm mx-4 btn-primary' style:'float:right'> Add to cart </button></a>
+             </div>
+           </div>
+           </div>
+     
+           <script>
+             function border(){
+                 var selector_box = Document.querySelector('.singleProduct');
+                 selector_box.classList.add('border-image');
+             }
+           </script>
+             
+             ";
+         }
+                all_pro_cart();
+             
+                 ?> 
+                
+              
+                  
+          
+              </div>
+            </div>  
+          </div>  
+             
+            
+        </div> 
+            
+             
 <!--
   
 </div> 
--- functions/functions.php
+++ functions/functions.php
@@ -468,7 +468,7 @@ function getWomenPro()
         echo "
            
             
-        <div class='col-sm-3 my-2 singleProduct' onmouseover='border()'>    
+        <div class='col-lg-3 my-2 singleProduct' onmouseover='border()'>    
         <div class='card' style='width: 18rem;'>
         <a href='details.php?pro_id=$pro_id'><img class='card-img-top' src='admin_area/product_images/$pro_image' alt='$pro_title image'  width='180' height='200'></a>
         <div class='card-body'>
@@ -489,7 +489,7 @@ function getWomenPro()
             var selector_box = Document.querySelector('.singleProduct');
             selector_box.classList.add('border-image');
         }
-      </script>
+      </script>     
         
         ";
     }
